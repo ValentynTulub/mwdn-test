b4b31c7a14dfd5bd16762e1c3092e8a8148241db
Valentyn Tulub <valentyn.tulub@gmail.com>
2023-05-29 19:48:46 +0300

    Implement the endpoint for fetching images

--- backend/src/images/images.module.ts
+++ backend/src/images/images.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ImagesService } from './images.service';
 import { ImagesController } from './images.controller';
+import {HttpModule} from "@nestjs/axios";
+import * as process from "process";
 
 @Module({
+  imports: [HttpModule.register({
+    baseURL: process.env.IMAGES_SOURCE || 'https://my-json-server.typicode.com/icedrone/json-demo-server'
+  }),],
   controllers: [ImagesController],
   providers: [ImagesService]
 })
--- backend/src/images/images.service.ts
+++ backend/src/images/images.service.ts
@@ -1,15 +1,23 @@
 import { Injectable } from '@nestjs/common';
 import { CreateImageDto } from './dto/create-image.dto';
 import { UpdateImageDto } from './dto/update-image.dto';
+import {HttpService} from "@nestjs/axios";
+import {firstValueFrom} from "rxjs";
 
 @Injectable()
 export class ImagesService {
+  constructor(private readonly httpService: HttpService) {}
+
   create(createImageDto: CreateImageDto) {
     return 'This action adds a new image';
   }
 
-  findAll() {
-    return `This action returns all images`;
+  async findAll() {
+    const [album1, album2] = await Promise.all([
+      firstValueFrom(this.httpService.get('/photos')),
+      firstValueFrom(this.httpService.get('/images'))
+    ]);
+    return [...album1.data, ...album2.data];
   }
 
   findOne(id: number) {
